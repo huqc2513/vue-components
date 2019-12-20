036cad07cf040f987f1df390e7dc52bb5bc14dce
Huqc <593989124@qq.com>
2019-12-21 00:14:58 +0800

    静态页面部署

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,8 @@
+console.log ('env', process.env.mode);
 let isDev = process.env.mode === 'dev';
 
 module.exports = {
   lintOnSave: false,
+  // publicPath:'./'
   publicPath: isDev ? './' : '/vue-components/',
 };
